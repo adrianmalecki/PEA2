4cb111494f7190d37f214b6a72140b1d02dfe717
unknown <adrian.malecki99@o2.pl>
2020-12-12 12:25:59 +0100

    Added SA

--- menu.py
+++ menu.py
@@ -6,27 +6,30 @@ from sa import SimulatedAnnealing
 class Menu:
     def __init__(self):
         self.read_data = Read_data()
-        self.sa = SimulatedAnnealing()
         self.nodes = []
         self.choice = 0
+        self.max_time = 0
+        self.a = 0
+        self.temperature = 0
 
     def main_menu(self):
         while self.choice != 6:
             print('_________MENU_________')
             print('1. Wczytaj dane')
-            print('2. Wprowadz kryterium stopu')
+            print('2. Wprowadz kryterium stopu(czas w sekundach)')
             print('3. Wybór sąsiedztwa')
             print('4. Tabu Search')
             print('5. Ustaw współczynnik zmainy temperatury dla SW')
-            print('6. SW')
-            print('7. Wyjście')
+            print('6. Ustaw temperature')
+            print('7. SW')
+            print('8. Wyjście')
 
             self.choice = input('Wybór: ')
 
             if self.choice == '1':
                 self.read_data.show_avaiable_files()
                 #file_name = input("Podaj nazwe pliku: ")
-                file_name = 'ftv10.atsp'
+                file_name = 'rbg403.atsp'
                 try:
                     open(format(file_name), "r")
                     self.nodes = self.read_data.read_file(file_name)
@@ -34,10 +37,20 @@ class Menu:
                 except IOError:
                     print("Error!")
 
+            elif self.choice == '2':
+                self.max_time = int(input("Podaj czas: "))
+            
+            elif self.choice == '5':
+                self.a = float(input("Podaj współczynnik schładzania: "))
+
             elif self.choice == '6':
-                self.sa.find_solution(self.nodes)
+                self.temperature = int(input("Podaj temperature początkową: "))
 
             elif self.choice == '7':
+                self.sa = SimulatedAnnealing(self.temperature, self.a, self.max_time)
+                best_path, best_sol, end_temp = self.sa.find_solution(self.nodes)
+                print(best_path, best_sol, end_temp)
+            elif self.choice == '8':
                 exit()
 
             else:
--- sa.py
+++ sa.py
@@ -1,22 +1,60 @@
 import copy
 import random
+from queue import *
+import copy
+import time
+import random
+import operator
+import math
 from sys import maxsize
-from read_data import Read_data
+
 
 class SimulatedAnnealing:
-    def __init__(self):
-        self.temperature = 60
-        self.free_nodes = []
+    def __init__(self, temperature, a, max_time):
+        self.temperature = temperature
+        self.best_path = maxsize
+        self.lowest_cost = maxsize
+        self.current_path = maxsize
+        self.current_cost = maxsize
+        self.max_time = max_time
+        self.a = a
         pass
 
-    def find_solution(self, nodes):
-        size = len(nodes)
+    def check_data_init(self):
+        if self.temperature == 0:
+            self.temperature = 1000
+        if self.a == 0:
+            self.a = 0.9
+        if self.max_time == 0:
+            self.max_time = 100
 
-        self.first_solution(nodes, size)
+    def find_solution(self, nodes):
+        self.check_data_init()
+        size = len(nodes)
+        total_time = 0
+        self.current_path, self.lowest_cost = self.first_solution(nodes, size)
+        self.best_path = self.current_path
+        while total_time < self.max_time:
+            start = time.process_time()
+            v1 = random.randint(0, size - 1)
+            new_cost, new_path = self.find_best_transformation(v1, nodes)
+
+            if new_cost < self.lowest_cost:
+                self.best_path = new_path
+                self.current_path = new_path
+                self.lowest_cost = new_cost
+            else:
+                probability = math.exp((self.lowest_cost - new_cost) / self.temperature)
+                if random.random() < probability:
+                    self.current_path = new_path
+            duration = time.process_time() - start
+            total_time += duration
+            self.temperature = self.temperature * self.a
+        return self.best_path, self.lowest_cost, self.temperature
 
     def first_solution(self, nodes, size):
         help_list = copy.deepcopy(nodes)
-        node = 8#random.randint(0, size)
+        node = random.randint(0, size - 1)
         path = []
         for i in range(0, size):
             help_list[i][node] = 100000000
@@ -27,17 +65,26 @@ class SimulatedAnnealing:
             node = help_list[node].index(min_value)
             for i in range(0, size):
                 help_list[i][node] = 100000000
-        print(path)
-        self.calculate_cost(nodes, path)
-        return path
+        cost = self.calculate_cost(nodes, path)
+        return path, cost
 
     def calculate_cost(self, nodes, path):
         cost = 0
         for i in range(1, len(path)):
-            cost += nodes[path[i-1]][path[i]]
-        cost += nodes[path[len(nodes)-1]][path[0]]
-        print('cost', cost)
+            cost += nodes[path[i - 1]][path[i]]
+        cost += nodes[path[len(nodes) - 1]][path[0]]
+        return cost
 
+    def swap(self, node1, node2, path):
+        path[node1], path[node2] = path[node2], path[node1]
 
-
-
+    def find_best_transformation(self, node, nodes):
+        queue = PriorityQueue()
+        size = len(nodes)
+        for i in range(0, size - 1):
+            if i != node:
+                transformed_path = copy.deepcopy(self.current_path)
+                self.swap(node, i, transformed_path)
+                transformed_cost = self.calculate_cost(nodes, transformed_path)
+                queue.put((transformed_cost, transformed_path))
+        return queue.get()
